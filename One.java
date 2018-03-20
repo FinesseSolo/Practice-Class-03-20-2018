69559cb7c7ea8ca5b42724e338bec116d3b4b7bf
Hancock <bchancock.2017@mymail.jcsu.edu>
2018-03-20 14:45:32 -0400

    first

---
+++ One.java
@@ -0,0 +1,16 @@
+ import java.util.Scanner;
+public class One {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scanner = new Scanner (System.in);
+System.out.println("Please enter a number");
+int A = scanner.nextInt();
+for(int i = 1; i <= 10; i++ ){
+	
+	System.out.println(A * i);
+	
+}
+	}
+
+}
---
+++ Two.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Two {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scanner = new Scanner (System.in);
+System.out.println("Please Enter Numbers");
+int N = scanner.nextInt();
+int A = scanner.nextInt();
+int B = scanner.nextInt();
+
+System.out.println(N + A * A + B * B *B);
+
+	
+}
+	}
+
